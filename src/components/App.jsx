1797faf72d4e0348a3ec37640ecec7409d9358ab
Denis <thewoodman999@gmail.com>
2022-06-04 13:14:31 +0300

    v 2.0.0

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,70 @@
+import { useState } from "react";
 import "./App.css";
 import { peopleFromAT } from "../database/database";
 
 function App() {
+  const [color, setColor] = useState();
+  const [dragItem, setDragItem] = useState();
+  const [list, setList] = useState(peopleFromAT);
+
+  const handleDragStart = (index) => {
+    setColor(true);
+    setDragItem(index);
+  };
+
+  const handleDragEnter = (e, index) => {
+    e.target.style.backgroundColor = "#afffd7";
+    const newList = [...list];
+    const item = newList[dragItem];
+    newList.splice(dragItem, 1);
+    newList.splice(index, 0, item);
+    setDragItem(index);
+    setList(newList);
+    setColor(true);
+  };
+
+  const handleDragLeave = (e) => {
+    e.target.style.backgroundColor = "white";
+  };
+
+  const handleDrop = (e) => {
+    setColor(false);
+    e.target.style.backgroundColor = "white";
+  };
+
   return (
-    <main>
-      {peopleFromAT.map(({ id, name, message, img }) => {
-        return (
-          <div className="main__card" key={id}>
-            <div className="card__img">
-              <img src={img} alt={name} />
-            </div>
-            <div className="card__info">
-              <div className="info__message">
-                <p>{message}</p>
-              </div>
-              <div className="info__id">
-                <p>({id})</p>
-              </div>
-              <div className="info__name">
-                <p>{name}</p>
-              </div>
-            </div>
-          </div>
-        );
-      })}
+    <main className={color ? "active" : ""}>
+      <ul>
+        {list &&
+          list.map(({ id, name, message, img }, index) => {
+            return (
+              // eslint-disable-next-line jsx-a11y/anchor-is-valid
+              <li
+                className="main__card"
+                draggable
+                key={index}
+                onDragStart={() => handleDragStart(index)}
+                onDragEnter={(e) => handleDragEnter(e, index)}
+                onDragLeave={(e) => handleDragLeave(e)}
+                onDrop={(e) => handleDrop(e)}
+                onDragOver={(e) => e.preventDefault()}
+              >
+                <div className="card__img">
+                  <img src={img} alt={name} />
+                </div>
+                <div className="card__info">
+                  <div className="info__message">
+                    <p>"{message}"</p>
+                  </div>
+                  <div className="info__bottom">
+                    <p className="info__id">id({id})</p>
+                    <p className="info__name">{name}</p>
+                  </div>
+                </div>
+              </li>
+            );
+          })}
+      </ul>
     </main>
   );
 }
--- src/database/database.js
+++ src/database/database.js
@@ -1,41 +1,48 @@
+import Finn from "../assets/Finn.png";
+import Jake from "../assets/Jake.png";
+import BMO from "../assets/BMO.png";
+import Marselin from "../assets/Marselin.png";
+import IceKing from "../assets/IceKing.png";
+import BubbleGum from "../assets/BubbleGum.png";
+
 export const peopleFromAT = [
   {
     id: 1,
     name: "Finn",
     message:
       "Вы — мои лучшие друзья!... Как же вы не поймете, что все мы в пролете, если мы не играем на одной ноте.",
-    img: "",
+    img: Finn,
   },
   {
     id: 2,
     name: "Jake",
     message: "Я хочу жениться на своей кровати.",
-    img: "",
+    img: Jake,
   },
   {
     id: 3,
     name: "BMO",
     message:
       "— БиМО, это жестокий и злой мир. Люди тут всегда творят безумства. — То есть некоторые люди — просто куча крысиного навоза, играющие на полную громкость и больше ничего?",
-    img: "",
+    img: BMO,
   },
   {
     id: 4,
     name: "Bubble Gum",
     message: "Конфетолюди особенные. Их надо няшкать и тетешкать.",
-    img: "",
+    img: BubbleGum,
   },
   {
     id: 5,
     name: "Marselin",
     message: "Он был слишком старомоден, чтобы жить. Не выдержал новых времён.",
-    img: "",
+    img: Marselin,
   },
   {
     id: 6,
     name: "Ice King",
     message:
       "— Я всегда думал, что умру, спасая кого-то... — Эй, никто не выбирает, как это случится.",
-    img: "",
+    img: IceKing,
   },
 ];
